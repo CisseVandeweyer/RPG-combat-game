b671bebda6176c58c96f610b3d6ea01af4e8030d
Cissevos <r0938854@student.thomasmore.be>
2025-02-07 01:48:13 +0100

    finish sectie 7

--- PPP/Assets/Scripts/SceneManagemant/Fader.cs
+++ PPP/Assets/Scripts/SceneManagemant/Fader.cs
@@ -5,9 +5,30 @@ namespace RPG.SceneManagement
 {
     public class Fader : MonoBehaviour
     {
-        IEnumerator FadeOut(float time)
+
+        CanvasGroup canvasGroup;
+
+        private void Start()
         {
-            yield break;
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
         }
 
     }
