5002f9de19c3f835f0562f6f1b1699f47ce4faf3
Cissevos <r0938854@student.thomasmore.be>
2025-02-08 22:19:45 +0100

    finish sectie 10.3

--- PPP/Assets/Scripts/Combat/Projectile.cs
+++ PPP/Assets/Scripts/Combat/Projectile.cs
@@ -9,16 +9,23 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1f;
+        [SerializeField] bool isHoming = true;
+        [SerializeField] GameObject hitEffect = null;
 
         Health target;
         float damage = 0;
+
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
 
         // Update is called once per frame
         void Update()
         {
             if (target == null) return;
+            if (isHoming && !target.IsDead()) transform.LookAt(GetAimLocation());
 
-            transform.LookAt(GetAimLocation());
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         }
@@ -40,7 +47,14 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Health>() != target) return;
+            if (target.IsDead()) return;
             target.TakeDamage(damage);
+
+            if (hitEffect != null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
+
             Destroy(gameObject);
         }
     }
